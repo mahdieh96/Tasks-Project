df433c8281f4026bb259d37e886d9f677dca5987
mahdie <jafari.mahdieh@gmail.com>
2023-08-24 15:57:31 +0330

    button component added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Button } from "./components/UI/Button";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Button>Add Task</Button>
     </div>
   );
 }
