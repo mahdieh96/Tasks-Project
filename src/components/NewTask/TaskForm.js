8eab4b7a31b8507a0ed88046e40390abbc5d7b02
mahdie <jafari.mahdieh@gmail.com>
2023-08-24 17:14:28 +0330

    validation for input added

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Input } from "../UI/Input";
 import { Button } from "../UI/Button";
 import classes from "./TaskForm.module.css";
 import { Card } from "../UI/Card";
 export const TaskForm = () => {
+  const inputRef = useRef();
+  const [error, setError] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (inputRef.current.value.trim().length === 0) {
+      setError(true);
+      return;
+    }
+  };
+  const inputChangeHandler = () => {
+    if (inputRef.current.value.trim().length > 0) {
+      setError(false);
+      return;
+    }
+    setError(true);
+  };
   return (
     <Card>
-      <form className={classes.form}>
-        <Input />
+      <form className={classes.form} onSubmit={submitHandler}>
+        <Input ref={inputRef} onChange={inputChangeHandler} />
         <Button type="submit">Add Task</Button>
       </form>
+      {error && <p className={classes.error}>input can not be empty</p>}
     </Card>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,5 +1,11 @@
 import React from "react";
 import classes from "./Input.module.css";
-export const Input = (props) => {
-  return <input className={`${classes.input} ${props.className || ""}`} />;
-};
+export const Input = React.forwardRef((props, ref) => {
+  return (
+    <input
+      className={`${classes.input} ${props.className || ""}`}
+      onChange={props.onChange}
+      ref={ref}
+    />
+  );
+});
